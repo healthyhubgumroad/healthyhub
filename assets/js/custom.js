c610a9cabfbb6d1ec130b2f06720fb00f05179ea
healthyhubgumroad <158469517+healthyhubgumroad@users.noreply.github.com>
2024-02-01 14:52:40 +0200

    Add files via upload

---
+++ assets/js/custom.js
@@ -0,0 +1,23 @@
+$("#buttonarrow").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#myDiv").offset().top
+    }, 1000);
+});
+
+$("#button").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#myDiv").offset().top
+    }, 1000);
+});
+
+$("#button2").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#myDiv").offset().top
+    }, 1000);
+});
+
+$("#button3").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#myDiv").offset().top
+    }, 1000);
+});
